92e95563a6ca828be039b2437f2effa866f3ab12
chrystopher1989mr <salvador89_1@hotmail.com>
2014-02-04 22:53:09 -0600

    version 1.1 arquinaturavalle.com.mx

---
+++ index.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Arquinatura</title>
+
+    
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+  
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/blueimp-gallery.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-image-gallery.min.css">
+  </head>
+
+  <body>
+
+    <!--  Menu -->
+    <a id="menu-toggle" href="#" class="btn btn-primary btn-lg toggle"><i class="fa fa-bars"></i></a>
+    <div id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <a id="menu-close" href="#" class="btn btn-default btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
+        <li class="sidebar-brand"><a href="#">Arquinatura</a></li>
+        <li><a href="#top">Inicio</a></li>
+        <li><a href="#about">Nosotros</a></li>
+        <li><a href="#services">Servicios</a></li>
+        <li><a href="#portfolio">Portafolio</a></li>
+        <li><a href="#contact">Contacto</a></li>
+      </ul>
+    </div>
+    <!-- / Menu -->
+  
+    <!--  -->
+    <div id="top" class="header">
+      <div class="vert-text">
+
+
+<img src="img/logo.png" width="700px" class="visible-md visible-lg text-center center-block">
+
+
+        
+          <img src="img/logo.png" width="230px" class="visible-xs text-center center-block">
+       
+      
+        <!--<h3>La arquitectura moderna no significa el uso de nuevos materiales, sino utilizar los materiales existentes en una forma más humana.</h3>-->
+        <br>
+        <a href="#about" class="btn btn-default btn-lg">Conoce más</a>
+      </div>
+    </div>
+    <!-- / -->
+  
+    <!-- Intro -->
+    <div id="about" class="intro">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3 text-center">
+            <h2>Quienes Somos ?</h2>
+            <hr>
+            <h4>Somos un grupo de Arquitectos e Ingenieros capacitados, para dar respuestas creativas, a la necesidad de una vivienda, conjunto habitacional o cualquier espacio. Siempre con el compromiso, el respeto y la inclusión de técnicas amigables con el medio ambiente.
+
+             </h4><hr>
+              <h4>El diseño urbano tanto como el diseño de viviendas a diferentes escalas es el mayor compromiso que tenemos con la sociedad, siempre respetando el pasado pero proyectando hacia el futuro.</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /Intro -->
+   
+    <!-- Servicios -->
+    <div id="services" class="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3 text-center">
+            <h2>Nuestros Servicios</h2>
+            <hr>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 text-center">
+            <div class="service-item">
+              <img  src="img/residencial.jpg" width="150px" class="img-circle">
+              <h4>CONSTRUCCIÓN</h4>
+              <p>Residencial, Desarrollos, Condominios, Plazas Comerciales, Supervición y Administración, Presupuestos y Control Presupuestal.</p>
+            </div>
+          </div>
+          <div class="col-md-6  text-center">
+            <div class="service-item">
+              <img  src="img/urbano.jpg" width="150px" class="img-circle">
+              <h4>DISEÑO URBANO</h4>
+              <p>
+                Desarrollos y Condominios.
+              </p>
+            </div>
+          </div>            
+        </div>
+        <div class="row">
+          
+          
+          <div class="col-md-6  text-center">
+            <div class="service-item">
+             <img  src="img/comercial.jpg" width="150px" class="img-circle">
+              <h4>DISEÑO CONCEPTUAL</h4>
+              <p>
+                Plan de Negocios de Proyectos Inmobiliarios.
+              </p>
+            </div>
+          </div>
+        
+          <div class="col-md-6 text-center">
+            <div class="service-item">
+              <img  src="img/sustentable.jpg" width="150px" class="img-circle">
+              <h4>DISEÑO  ARQUITECTÓNICO  SUSTENTABLE</h4>
+              <p>Bajo Criterios Bio-Climático y Bio-Ético, Residencial, Desarrollos, Condominios, Plazas Comerciales, Spa´s y Centros de Salud Integral.</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /Servicios -->
+
+    <!-- Callout -->
+    <div class="callout">
+      <div class="vert-text">
+        <h1>El muro no sabía que era muro, hasta que conoció la luz.</h1>
+        
+      </div>
+    </div>
+    <!-- /Callout -->
+
+    <!---->
+
+    <!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
+<div id="blueimp-gallery" class="blueimp-gallery">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <!-- Controls for the borderless lightbox -->
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->
+    <div class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body next"></div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left prev">
+                        <i class="glyphicon glyphicon-chevron-left"></i>
+                        
+                    </button>
+                    <button type="button" class="btn btn-primary next">
+                        
+                        <i class="glyphicon glyphicon-chevron-right"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!---->
+    <!-- Portfolio -->
+
+
+    <div id="portfolio" class="portfolio">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-md-offset-4 text-center">
+            <h2>Nuestro Trabajo</h2>
+            <hr>
+          </div>
+        </div>
+        <div id="links" class="row">
+        <?php 
+       for ($i=1; $i < 21; $i++) { 
+      
+       
+        ?>
+         <div class="col-md-2  text-center">
+            <div class="portfolio-item">
+              <a href="img/trabajos/<?php echo $i?>.jpg" data-gallery><img class="img-portfolio img-responsive" src="img/trabajos/<?php echo $i?>.jpg"></a>
+            </div>
+          </div>
+        <?php
+      }
+        ?>
+         
+
+         
+         
+        </div>
+       
+        
+      </div>
+    </div>
+    <!-- /Portfolio -->
+
+    <!-- Call to Action -->
+    <div id="contact" class="call-to-action">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3 text-center">
+            <h3>Tienes un proyecto en mente, comunícate con nosotros</h3>
+            <hr>
+            <p><i class="fa fa-phone fa-3x"></i><strong> <h4>01-726-26-2-62-22</h4></strong></p>
+          <form name="sentMessage" class="well" id="contactForm"  novalidate>
+         
+     <div class="control-group">
+                    <div class="controls">
+      <input type="text" class="form-control" 
+             placeholder="Nombre" id="name" required
+                 data-validation-required-message="Por favor Ingresa Tu Nombre" />
+        <p class="help-block"></p>
+       </div>
+
+           </div> 
+           <hr>  
+                <div class="control-group">
+                  <div class="controls">
+      <input type="email" class="form-control" placeholder="Correo Electronico" 
+                      id="email" required
+               data-validation-required-message="Por favor Ingresa Tu Correo Electronico" />
+    </div>
+      </div>  
+        <hr>
+      <div class="control-group">
+                  <div class="controls">
+      <input type="text" class="form-control" placeholder="Teléfono" 
+                      id="telefono" required
+               data-validation-required-message="Por favor Ingresa tu Teléfono" />
+    </div>
+      </div>  
+<hr>
+               <div class="control-group">
+                 <div class="controls">
+         <textarea rows="10" cols="100" class="form-control" 
+                       placeholder="Mensaje" id="message" required
+           data-validation-required-message="Por favor Ingresa tu Mensaje" minlength="5" 
+                       data-validation-minlength-message="Minimo 5 caracteres" 
+                       maxlength="999" style="resize:none"></textarea>
+    </div>
+               </div>      
+       <div id="success"> </div> <!-- For success/fail messages -->
+      <button type="submit" class="btn btn-primary pull-right">Enviar</button><br />
+      <br/>
+          </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /Call to Action -->
+
+ 
+    
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3 text-center">
+            <ul class="list-inline">
+              <li><a href="https://www.facebook.com/arquinaturavalle" target="_blank"><i class="fa fa-facebook fa-3x"></i></a></li>       
+            </ul>
+            <div class="top-scroll">
+              <a href="#top"><i class="fa fa-circle-arrow-up scroll fa-4x"></i></a>
+            </div>
+            <hr>
+            <p>Copyright &copy; Arquinatura 2013 - 2014</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- /Footer -->
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrapvalidation.js"></script>
+    <script src="js/contactme.js"></script>
+    <script src="js/jquery.blueimp-gallery.min.js"></script>
+    <script src="js/bootstrap-image-gallery.min.js"></script>
+
+    <!-- Custom JavaScript for the Side Menu and Smooth Scrolling -->
+    <script>
+        $("#menu-close").click(function(e) {
+            e.preventDefault();
+            $("#sidebar-wrapper").toggleClass("active");
+        });
+    </script>
+    <script>
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#sidebar-wrapper").toggleClass("active");
+        });
+    </script>
+    <script>
+      $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+    </script>
+
+  </body>
+
+</html>
